7cbf257743dcebfdf99f2a3d9bca3fc52232136a
balthier7997 <36052237+balthier7997@users.noreply.github.com>
2018-02-02 12:56:53 +0100

    Add files via upload

---
+++ BCFrame.java
@@ -0,0 +1,8 @@
+import javax.swing.JFrame;
+
+@SuppressWarnings("serial")
+public class BCFrame extends JFrame {
+
+	
+	
+}
---
+++ BCListener.java
@@ -0,0 +1,11 @@
+import java.awt.event.*;
+
+public class BCListener implements ActionListener {
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
